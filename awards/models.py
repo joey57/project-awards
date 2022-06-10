57f61abe57e4ac49b7eeeb8213299876fd64ce1c
joey57 <njorogejoey5@gmail.com>
2022-06-10 17:50:54 +0300

    create profile model and apply migrations

--- awards/models.py
+++ awards/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+import datetime as dt
 
 # Create your models here.
+class Profile(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  image = CloudinaryField('profile_pics/',default='default.jpg')
+  bio = models.TextField(max_length=500, default='My Bio', blank=True)
+  name = models.CharField(max_length=250, blank=True)
+
+  def __str__(self):
+    return f'{self.user.username} Profile'
