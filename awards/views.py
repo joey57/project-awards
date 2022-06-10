dd817e48c15d499503e7c8b20e20ca85cd0e86d9
joey57 <njorogejoey5@gmail.com>
2022-06-10 18:53:46 +0300

    add update profile and create profile views

--- awards/views.py
+++ awards/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import redirect, render
-from django.http  import HttpResponse, Http404
+from django.http  import HttpResponse, Http404, HttpResponseRedirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm, UpdateProfile, CreateProfileForm
 from django.contrib.auth.decorators import login_required
+from .models import Profile, User
 
 # Create your views here.
 
@@ -24,23 +25,63 @@ def register(request):
        form = UserRegisterForm()
     return render(request, 'users/register.html',{"form":form})   
 
-@login_required
-def profile(request):
-  if request.method == 'POST':
-    u_form = UserUpdateForm(request.POST, instance=request.user)
-    p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
-    if u_form.is_valid() and p_form.is_valid():
-      u_form.save()
-      p_form.save()
-      messages.success(request, f'Your account has been updated')
-      return redirect('profile')
-  else:
-    u_form = UserUpdateForm(instance=request.user)
-    p_form = ProfileUpdateForm(instance=request.user.profile)
+# @login_required
+# def profile(request):
+#   if request.method == 'POST':
+#     u_form = UserUpdateForm(request.POST, instance=request.user)
+#     p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+#     if u_form.is_valid() and p_form.is_valid():
+#       u_form.save()
+#       p_form.save()
+#       messages.success(request, f'Your account has been updated')
+#       return redirect('profile')
+#   else:
+#     u_form = UserUpdateForm(instance=request.user)
+#     p_form = ProfileUpdateForm(instance=request.user.profile)
 
   
-  context ={
-    'u_form': u_form,
-    'p_form': p_form
-  }
-  return render(request, 'users/profile.html', context)
+#   context ={
+#     'u_form': u_form,
+#     'p_form': p_form
+#   }
+#   return render(request, 'users/profile.html', context)
+@login_required
+def profile(request, username):
+    '''
+    Method to display a specific user profile
+    '''
+    profile = Profile.get_user(username)
+   
+    
+    return render(request, 'users/profile.html', {"profile": profile })
+
+@login_required
+def create_profile(request):
+  current_user=request.user
+  if request.method == 'POST':
+    form = CreateProfileForm(request.POST,request.FILES)
+    if form.is_valid():
+      profile = form.save(commit=False)
+      profile.user = current_user
+      profile.save()
+    return HttpResponseRedirect('/')
+  else:
+    form = CreateProfileForm()
+  return render(request,'user/create_profile.html',{"form":form})
+
+@login_required
+def update_profile(request,username):
+  user=User.objects.get(username=username)
+  current_user = request.user
+  
+  if request.method =='POST':
+    form = UpdateProfile(request.POST,request.FILES, instance=current_user.profile)
+    
+    if form.is_valid():
+      form.save()
+      return redirect('profile', user.username)
+  
+  else:
+    form = UpdateProfile(instance=current_user.profile)
+  
+  return render(request,"users/update_profile.html", {"form":form})
