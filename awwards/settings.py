d6b255558c741090295131740753d821adb37751
joey57 <njorogejoey5@gmail.com>
2022-06-13 08:38:43 +0300

    add auth token to settings

--- awwards/settings.py
+++ awwards/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'cloudinary',
     'crispy_forms',
     'rest_framework',
+    'rest_framework.authtoken',
 
 ]
 
@@ -112,6 +113,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    )
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
