8bbd3e2311c6373a9b77e06d1d679a56f0e85bbe
joey57 <njorogejoey5@gmail.com>
2022-06-13 08:39:02 +0300

    create url to handle authentication

--- awwards/urls.py
+++ awwards/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from awards import views as awards_views
 from django.contrib.auth import views as auth_views
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,6 +27,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name = 'login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name = 'logout'),
     path('profile/<str:username>/', awards_views.profile, name = 'profile'),
+    path('api-token-auth/', obtain_auth_token)
 
     
 ]
